ab09791e21418fb434bacc11e82b92b18272c8fe
ashishharswal-dev <113756439+ashishharswal-dev@users.noreply.github.com>
2023-09-07 12:24:08 +0530

    Add files via upload

---
+++ src/components/ActiveJobs.jsx
@@ -0,0 +1,12 @@
+const ActiveJobs = props => {
+    return (
+      <div className="activeJobs">
+        <div className="activeJobsContainer">
+          <span className="activeJobsNum">5</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default ActiveJobs;
+  
---
+++ src/components/JobCredits.jsx
@@ -0,0 +1,12 @@
+const JobCredits = props => {
+    return (
+      <div className="jobCredits">
+        <div className="jobCreditsContainer">
+          <span className="jobCreditsNum">850</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default JobCredits;
+  
